2132cd1c95c7b5986bf2a3ae623837f1d06bcbbc
naaoufal <naoufelbenmensour@gmail.com>
2024-05-06 11:20:09 +0100

    remove unused tags

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import TopBar from '../../components/TopBar';
-import NavBar from "../../components/NavBar";
+// import TopBar from '../../components/TopBar';
+// import NavBar from "../../components/NavBar";
 import Carousel from "../../components/Carousel";
 import Courses from "../../components/Courses";
-import ResponsiveBar from "../../components/ResponsiveBar";
+// import ResponsiveBar from "../../components/ResponsiveBar";
 import Insctructors from "../../components/Instructors";
-import Footer from "../../components/Footer";
+// import Footer from "../../components/Footer";
 import EventCard from "../../components/EventCard";
 
 const HomeScreen = () => {
